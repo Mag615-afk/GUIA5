0e2d42524523d3e6263f71cef2ffed92776bc22d
k <022200520c@uandina.edu.pe>
2025-09-25 17:11:37 -0500

    Actividad 1

---
+++ lib/libros.dart
@@ -0,0 +1,14 @@
+class Libro{
+  int? id;
+  String tituloLibro;
+
+  Libro({this.id, required this.tituloLibro});
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'tituloLibro': tituloLibro,
+    };
+  }
+}
+
